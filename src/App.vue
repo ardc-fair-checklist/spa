1370aa7076b63040385c27679d60647e36ca07e8
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2022-09-06 19:10:58 +0200

    working on a minimal version, adding the questions and answers

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <Question v-for="{id, question, answers} in questions"
+        v-bind:answers="answers"
+        v-bind:id="id"
+        v-bind:question="question"
+        key="id"
+    />
+</template>
+
+
+<script setup lang="ts">
+import Question from './components/Question.vue'
+import questions from './assets/questions.json'
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/Question.vue
@@ -0,0 +1,25 @@
+<template>
+    <fieldset>
+        <legend>{{ question }}</legend>
+
+        <div v-for="{answer, id} in answers" >
+            <input type="radio" id="louie" value="louie" >
+            <label for="louie">{{ answer }}</label>
+        </div>
+
+    </fieldset>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+    answers: {
+        answer: string,
+        id: string
+    }[]
+    id: string
+    question: string
+}>()
+</script>
+
+<style scoped>
+</style>
