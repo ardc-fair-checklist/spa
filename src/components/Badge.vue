da297eaec43360ad1cd760dab5b50d876f341bee
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2022-09-07 16:41:32 +0200

    Badge component had multiple roots; reactivity of the badge link was broken

--- src/components/Badge.vue
+++ src/components/Badge.vue
@@ -1,18 +1,18 @@
 <template>
-    <h2>Get the badge</h2>
-    <h3>Markdown</h3>
-    {{ badge.markdown }}
+    <div>
+        <h2>Get the badge</h2>
+        <h3>Markdown</h3>
+        <p>{{ badge }}</p>
+    </div>
 </template>
 
 <script setup lang="ts">
+import { computed } from 'vue'
 import { state } from '../store'
 import { HOST_AND_ROUTE } from '../constants'
 import metadata from '../../package.json'
 const [major, minor] = metadata.version.split('.').slice(0, 2)
-
-const badge = {
-    markdown: `[![FAIRness badge image](${HOST_AND_ROUTE}/badge.svg)](${HOST_AND_ROUTE}?state="${state.value}"&version="${major}.${minor}")`
-}
+const badge = computed(() => `[![FAIRness badge image](${HOST_AND_ROUTE}/badge.svg)](${HOST_AND_ROUTE}?state=${state.value}&version=${major}.${minor})`)
 </script>
 
 <style scoped>
