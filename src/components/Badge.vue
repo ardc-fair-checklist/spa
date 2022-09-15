af6d581a65004810bfd3fcb102c7f65dab0e9fcc
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2022-09-15 14:32:15 +0200

    removed unused import of slices; now using newly exported fairQueryParams from store; moved styles to global file

--- src/components/Badge.vue
+++ src/components/Badge.vue
@@ -8,22 +8,15 @@
 
 <script setup lang="ts">
 import { computed } from 'vue'
-import { compliance } from '../store'
+import { fairQueryParams } from '../store'
 import { APP_BASE_URL } from '../constants'
 import metadata from '../../package.json'
-import { slices } from '../read-questions'
 const [major, minor] = metadata.version.split('.').slice(0, 2)
 const badge = computed(() => `[![FAIRness badge image](${APP_BASE_URL}/badge.svg)]` +
                              `(${APP_BASE_URL}/#/software?` +
                              `v=${major}.${minor}` +
-                             `&f=${compliance.value.slice(...slices.f).map(elem => elem.toString()).join('')}` +
-                             `&a=${compliance.value.slice(...slices.a).map(elem => elem.toString()).join('')}` +
-                             `&i=${compliance.value.slice(...slices.i).map(elem => elem.toString()).join('')}` +
-                             `&r=${compliance.value.slice(...slices.r).map(elem => elem.toString()).join('')}`)
+                             fairQueryParams.value)
 </script>
 
 <style scoped>
-.badge {
-    padding-bottom: 10em;
-}
 </style>
