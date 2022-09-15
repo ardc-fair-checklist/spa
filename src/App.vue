bb2428529de95f5c022dd04f93ee8b0ceedb319f
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2022-09-15 14:34:01 +0200

    no longer using Header component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 <template>
-    <Header />
     <!--
     Any meaningful work is delegated to the routed components, 
     which are inserted here by the router
@@ -10,7 +9,6 @@
 
 <script setup lang="ts">
 import Footer from './components/Footer.vue'
-import Header from './components/Header.vue'
 </script>
 
 <style scoped>
