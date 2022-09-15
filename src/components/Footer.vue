92e6ef74a94790ec40190cf04f8bccf1a2fbbb21
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2022-09-15 14:33:30 +0200

    replaced placeholder footer text with actual logos

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,13 @@
 <template>
     <footer>
-        &lt;footer placeholder&gt;
+        <div class="logos">
+            <img class="logo" src="netherlands-escience-center-logo.svg" alt="nlesc logo" />
+            <img class="logo" src="ardc-logo.svg" alt="ardc logo" />
+        </div>
+        <p>
+            This checklist is a collaborative effort by Netherlands eScience Center and Australian Research Data Commons.
+        </p>
+
     </footer>
 </template>
 
