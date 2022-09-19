7f4388a47b42a46dddf86604ab1cd90eb008e2ea
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2022-09-19 15:53:42 +0200

    added testing configuration with vitest, coverage-c8, jsdom, and @vue/test-utils

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
     base: "/ardc-fair-checklist/",
-    plugins: [vue()]
+    plugins: [vue()],
+    test: {
+        environment: 'jsdom'
+    }
 })
