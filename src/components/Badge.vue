4299c17ca9abd5d1fadc46b563484c1017eeba49
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2022-09-19 11:48:23 +0200

    now offering markdown, html and rst format badges

--- src/components/Badge.vue
+++ src/components/Badge.vue
@@ -2,7 +2,11 @@
     <div class="badge">
         <h2>Get the badge</h2>
         <h3>Markdown</h3>
-        <p>{{ badge }}</p>
+        <p>{{ badge.markdown.value }}</p>
+        <h3>ReStructured Text</h3>
+        <p>{{ badge.rst.value }} </p>
+        <h3>HTML</h3>
+        <p>{{ badge.html.value }}</p>
     </div>
 </template>
 
@@ -12,11 +16,20 @@ import { fairQueryParams } from '../store'
 import { APP_BASE_URL } from '../constants'
 import metadata from '../../package.json'
 const [major, minor] = metadata.version.split('.').slice(0, 2)
-const badge = computed(() => `[![FAIRness badge image](${APP_BASE_URL}/badge.svg)]` +
+const badge = {
+    html: computed(() => `<a href="${APP_BASE_URL}/#/software?` +
+                         `v=${major}.${minor}` +
+                         `${fairQueryParams.value}">` +
+                         `<img src="${APP_BASE_URL}/badge.svg" ` +
+                         `alt="FAIR checklist badge"></a>`),
+    markdown: computed(() => `[![FAIRness badge image](${APP_BASE_URL}/badge.svg)]` +
                              `(${APP_BASE_URL}/#/software?` +
                              `v=${major}.${minor}` +
-                             fairQueryParams.value)
+                             `${fairQueryParams.value}`),
+    rst: computed(() => `<rst badge placeholder>`)
+}
 </script>
+
 
 <style scoped>
 </style>
