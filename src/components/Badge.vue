74967506b94ceeb257261d1ec93bd5b4dbaf860f
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2022-09-21 12:48:39 +0200

    added rst badge textarea

--- src/components/Badge.vue
+++ src/components/Badge.vue
@@ -2,11 +2,11 @@
     <div class="badge">
         <h2>Get the badge</h2>
         <h3>Markdown</h3>
-        <p>{{ badge.markdown.value }}</p>
+        <textarea readonly wrap="off">{{ badge.markdown.value }}</textarea>
         <h3>ReStructured Text</h3>
-        <p>{{ badge.rst.value }} </p>
+        <textarea readonly wrap="off">{{ badge.rst.value }} </textarea>
         <h3>HTML</h3>
-        <p>{{ badge.html.value }}</p>
+        <textarea readonly wrap="off">{{ badge.html.value }}</textarea>
     </div>
 </template>
 
@@ -19,17 +19,27 @@ const [major, minor] = metadata.version.split('.').slice(0, 2)
 const badge = {
     html: computed(() => `<a href="${APP_BASE_URL}/#/software?` +
                          `v=${major}.${minor}` +
-                         `${fairQueryParams.value}">` +
-                         `<img src="${APP_BASE_URL}/badge.svg" ` +
-                         `alt="FAIR checklist badge"></a>`),
+                         `${fairQueryParams.value}">\n` +
+                         `  <img src="${APP_BASE_URL}/badge.svg" ` +
+                         `alt="FAIR checklist badge">\n</a>`),
     markdown: computed(() => `[![FAIRness badge image](${APP_BASE_URL}/badge.svg)]` +
                              `(${APP_BASE_URL}/#/software?` +
                              `v=${major}.${minor}` +
-                             `${fairQueryParams.value}`),
-    rst: computed(() => `<rst badge placeholder>`)
+                             `${fairQueryParams.value})`),
+    rst: computed(() => `.. image:: ${APP_BASE_URL}/badge.svg\n` +
+                        `   :target: ${APP_BASE_URL}/#software?v=${major}.${minor}${fairQueryParams.value}\n` +
+                        `   :alt: FAIR checklist badge`)
 }
 </script>
 
 
 <style scoped>
+textarea {
+    width: 100%;
+    height: 6em;
+    box-sizing: border-box;
+    border: 2px solid #ccc;
+    border-radius: 3px;
+    resize: none;
+}
 </style>
