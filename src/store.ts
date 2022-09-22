2d83c8f3c5b95eaf4a6f49c24c9fff323f65b082
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2022-09-22 17:31:10 +0200

    defined variables in smaller scope

--- src/store.ts
+++ src/store.ts
@@ -1,16 +1,23 @@
 import { computed, ref } from 'vue'
 import { questions, nQuestions, slices, nPointsMax } from './read-questions'
 
-const scoreArrays = questions.map(q => q.answers.map(a => a.score))
+
 const state = ref<number[]>(new Array(nQuestions.total).fill(0))
 
 export const compliance = computed(() => state.value);
-export const setCompliance = (newCompliance: number[]) => state.value = newCompliance
 
-const summation = (previousValue: number, currentValue: number) => previousValue + currentValue;
-const scores = computed(() => state.value.map((iAnswer, iQuestion) => scoreArrays[iQuestion][iAnswer]))
+export const fairQueryParams = computed(() =>
+    `&f=${compliance.value.slice(...slices.f).map(elem => elem.toString()).join('')}` +
+    `&a=${compliance.value.slice(...slices.a).map(elem => elem.toString()).join('')}` +
+    `&i=${compliance.value.slice(...slices.i).map(elem => elem.toString()).join('')}` +
+    `&r=${compliance.value.slice(...slices.r).map(elem => elem.toString()).join('')}`)
 
 export const progress = computed(() => {
+    const scores = computed(() => {
+        const scoreArrays = questions.map(q => q.answers.map(a => a.score))
+        return state.value.map((iAnswer, iQuestion) => scoreArrays[iQuestion][iAnswer])
+    })
+    const summation = (previousValue: number, currentValue: number) => previousValue + currentValue;
     return {
         f: `${100 * scores.value.slice(...slices.f).reduce(summation, 0) / nPointsMax.f}%`,
         a: `${100 * scores.value.slice(...slices.a).reduce(summation, 0) / nPointsMax.a}%`,
@@ -20,8 +27,4 @@ export const progress = computed(() => {
     }
 })
 
-export const fairQueryParams = computed(() =>
-    `&f=${compliance.value.slice(...slices.f).map(elem => elem.toString()).join('')}` +
-    `&a=${compliance.value.slice(...slices.a).map(elem => elem.toString()).join('')}` +
-    `&i=${compliance.value.slice(...slices.i).map(elem => elem.toString()).join('')}` +
-    `&r=${compliance.value.slice(...slices.r).map(elem => elem.toString()).join('')}`)
+export const setCompliance = (newCompliance: number[]) => state.value = newCompliance
